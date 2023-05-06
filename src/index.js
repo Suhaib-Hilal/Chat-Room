99310f901889f56fb95004fb47ba387b90f2f095
Suhaib-Hilal <tantary.suhaib.main@gmail.com>
2023-05-07 00:11:16 +0530

    Make your own chat room

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
-function checkRequirements() {
-    let username = document.querySelector(".username")
-    
+function isValidUsername() {
+    let usernameField = document.querySelector(".username-field")
+    let infoPara = document.querySelector(".info-para")
+
     // Check if the user has entered their name
-    if (username.value == "") {
-        let info = document.querySelector(".info")
-        info.innerHTML = "Username must not be empty"
-        username.focus()
+    if (usernameField.value == "") {
+        infoPara.innerHTML = "Username must not be empty"
+        usernameField.focus()
     }
-    else if (username.value.length < 3) {
-        let info = document.querySelector(".info")
-        info.innerHTML = "Username must contain atleast 3 letters"
-        username.focus()
+
+    // Check if the user has entered a valid name
+    else if (usernameField.value.length < 3) {
+        infoPara.innerHTML = "Username must contain atleast 3 letters"
+        usernameField.focus()
     }
     else {
-        return "Requirement's Satisfied"
+        return true
     }
 }
 
+function randomNumber(length = 6) {
+    return Math.floor(
+        Math.pow(10, length-1) + Math.random() * (Math.pow(10, length) - Math.pow(10, length-1) - 1)
+    );
+}
+
 function createRoom() {
-    if (checkRequirements() == "Requirement's Satisfied") {
-        window.location.assign("/public/pages/room_creation.html")
+
+    // Check if the username is valid
+    if (isValidUsername()) {
+        let homeCard = document.querySelector(".home-card")
+        let roomCreationCard = document.querySelector(".room-creation-card")
+        let roomId = document.querySelector(".room-id > .id")
+
+        // Activate the room creation card
+        homeCard.classList.remove("active")
+        roomCreationCard.classList.add("active")
+
+        // Generate a unique room id and add it to the website
+        id = randomNumber()
+        roomId.innerHTML = id
     }
 }
